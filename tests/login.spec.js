e4a76c561aa1d8fb16001602c3f75f7930379c6e
Sahil Alam <sahil.nabagram@gmail.com>
2026-05-18 11:25:22 +0530

    Added login page object and initial authentication test

---
+++ tests/login.spec.js
@@ -0,0 +1,33 @@
+class LoginPage {
+
+    constructor(page) {
+        this.page = page;
+
+        this.signupLoginBtn = page.getByRole('link', { name: 'Signup / Login' });
+
+        this.emailInput = page.locator('input[data-qa="login-email"]');
+
+        this.passwordInput = page.locator('input[data-qa="login-password"]');
+
+        this.loginBtn = page.getByRole('button', { name: 'Login' });
+
+        this.logoutBtn = page.getByRole('link', { name: 'Logout' });
+    }
+
+    async goto() {
+        await this.page.goto('https://automationexercise.com/');
+    }
+
+    async openLoginPage() {
+        await this.signupLoginBtn.click();
+    }
+
+    async login(email, password) {
+        await this.emailInput.fill(email);
+        await this.passwordInput.fill(password);
+        await this.loginBtn.click();
+    }
+
+}
+
+module.exports = LoginPage;
